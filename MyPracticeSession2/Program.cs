087377b16f02dbfcc51ac42d7cff35026636c1c6
Itscosmicjay <Itscosmicjay@gmail.com>
2020-09-29 17:27:46 +0100

    float and decimal variables duely represented

--- MyPracticeSession2/Program.cs
+++ MyPracticeSession2/Program.cs
@@ -7,6 +7,11 @@ namespace MyPracticeSession2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            const float Pi = 3.14f;
+            float number = 1.2f;
+            decimal number = 1.2m;
+
+
         }
     }
 }
