5e0f92d91a0ddd88118acbc4e5f71011e82dcae2
Itscosmicjay <Itscosmicjay@gmail.com>
2020-09-29 18:51:54 +0100

    In cases where you try to carry out a conversiion and an error which causes the program tocrash occurs(an exception) the exeption can be handled by using a try and catch instruction.So instead of the program crashing a message showing the error pops up

--- MyPracticeSession2/Program.cs
+++ MyPracticeSession2/Program.cs
@@ -28,11 +28,20 @@ namespace MyPracticeSession2
             int i = b;
             int e = 1000;
             byte c =(byte) e;
-            string number2 = "1234";
-            int d = Convert.ToInt32(number2);
+           
             Console.WriteLine(i);
             Console.WriteLine(e);
-            Console.WriteLine(d);
+            
+            try
+            {
+                string number2 = "1234";
+                byte d = Convert.ToByte(number2);
+                Console.WriteLine(d);
+            }
+            catch(Exception)
+            {
+
+            }
              
             Console.ReadLine();
         }
